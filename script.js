8db6fe6ec74b1babfe179f696244f68156a11e26
hannoveran <mamcukanna@gmail.com>
2024-11-14 00:15:35 +0200

    ЛР6

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let selectedTask = null;
 
-function selectTask(taskNumber) {
+
+// lab5
+function selectTaskLab5(taskNumber) {
     selectedTask = taskNumber;
 
-    task(taskNumber);
+    taskLab5(taskNumber);
 }
 
-function task(taskNumber) {
+function taskLab5(taskNumber) {
 
     switch(taskNumber) {
         case 1:
@@ -166,4 +168,189 @@ document.getElementById("editorText").addEventListener("input", updateFormattedT
 
 function updateFormattedText() {
     document.getElementById("formattedText").innerText = document.getElementById("editorText").value;
+}
+
+
+
+// Lab6
+function selectTaskLab6(taskNumber) {
+    selectedTask = taskNumber;
+
+    taskLab6(taskNumber);
+}
+
+function taskLab6(taskNumber) {
+    switch(taskNumber) {
+        case 2:
+            console.log("Executing Task 2");
+            const shirts = [
+                { productId: 1, name: 'shirt1', color: 'red', price: 100, },
+                { productId: 2, name: 'shirt2', color: 'blue', price: 200, },
+                { productId: 3, name: 'shirt3', color: 'orange', price: 300, },
+                { productId: 4, name: 'shirt4', color: 'black', price: 400, }
+            ];
+
+            
+            function getProductDetails(array, propertyName, valueToFind, success, error) {
+                const foundProduct = array.find(obj => obj[propertyName] == valueToFind);
+                if (foundProduct) return success(foundProduct)
+                else error();
+            }
+
+            function successCallback(product) {
+                return product;
+            }
+
+            function errorCallback() {
+                console.log("Такого товару не існує!");
+            }
+
+            let id = prompt("id?", '');
+            const result = getProductDetails(shirts, 'productId', id, successCallback, errorCallback);
+            console.log(result);
+
+            break;
+        case 4:
+            console.log("Executing Task 4");
+            const concerts = {
+                Київ: new Date("2020-04-01"),
+                Умань: new Date("2025-07-02"),
+                Вінниця: new Date("2020-04-21"),
+                Одеса: new Date("2025-03-15"),
+                Хмельницький: new Date("2020-04-18"),
+                Харків: new Date("2025-07-10"),
+            };
+
+            const getFutureConcerts = concerts => {
+                const currentDate = new Date();
+
+                const futureConcerts = Object.entries(concerts)
+                .filter(([city, date]) => date > currentDate) 
+                .sort((a, b) => a[1] - b[1]) 
+                .map(([city]) => city);
+
+                return futureConcerts;
+            }
+
+            console.log(getFutureConcerts(concerts));
+            break;
+        case 6:
+            console.log("Executing Task 6");
+            
+            const medicines = [
+                { name: "Noshpa", price: 170 },
+                { name: "Analgin", price: 55 },
+                { name: "Quanil", price: 310 },
+                { name: "Alphacholine", price: 390 },
+            ];
+            
+            const discountedProducts = medicines.map((product, id) => {
+                const discountedProduct = {
+                    ...product,
+                    id: id + 1,
+                };
+
+                if (product.price > 300) {
+                    discountedProduct.price = product.price * 0.7 
+                }
+                
+                return discountedProduct;
+            });
+
+            console.log(discountedProducts);
+
+            break;
+        case 8:
+            console.log("Executing Task 8");
+            
+            function Storage(initialItems) {
+                this.items = initialItems;
+            }
+
+            Storage.prototype.getItems = function() {
+                return this.items;
+            };
+
+            Storage.prototype.addItem = function(item) {
+                this.items.push(item);
+            };
+
+            Storage.prototype.removeItem = function(item) {
+                const itemIndex = this.items.indexOf(item);
+                if (itemIndex !== -1) {
+                    this.items.splice(itemIndex, 1);
+                }
+            };
+
+
+            const arr = ["apple", "banana", "mango"];
+            const storage = new Storage(arr);
+
+            console.log(storage.getItems()); 
+            storage.addItem("orange");
+            console.log(storage.getItems()); 
+            storage.removeItem("banana");
+            console.log(storage.getItems());
+
+            break;
+        case 9:
+            console.log("Executing Task 9");
+
+            const tweets = [
+                { id: "000", likes: 5, tags: ["js", "nodejs"] },
+                { id: "001", likes: 2, tags: ["html", "css"] },
+                { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+                { id: "003", likes: 8, tags: ["css", "react"] },
+                { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+            ];
+
+            function countTags(tweets) {
+                const tagCounts = {};
+            
+                tweets.forEach(tweet => {
+                    tweet.tags.forEach(tag => {
+                        tagCounts[tag] = (tagCounts[tag] || 0) + 1;
+                    });
+                });
+            
+                return tagCounts;
+            }
+            
+            console.log(countTags(tweets));
+            
+            break;
+        case 10:
+            console.log("Executing Task 10");
+
+            function checkBrackets(str) {
+                const stack = []; 
+                const openingBrackets = "({["; 
+                const closingBrackets = ")}]"; 
+                
+                for (let char of str) {
+                    if (openingBrackets.includes(char)) {
+                        stack.push(char);
+                    } else if (closingBrackets.includes(char)) {
+                        const last = stack.pop(); 
+                        if (char === ')' && last !== '(' ||
+                            char === '}' && last !== '{' ||
+                            char === ']' && last !== '[') {
+                            return false; 
+                        }
+                    }
+                }
+                
+                return stack.length === 0;
+            }
+            
+            console.log(checkBrackets("function someFn() { console.log('test'); }")); 
+            console.log(checkBrackets("function someFn( { console.log('test'); ) }")); 
+            console.log(checkBrackets("function someFn() [ console.log('test'); ]")); 
+            console.log(checkBrackets("function someFn() { console.log('test');")); 
+            
+            
+            break;
+        default:
+            console.log("Invalid Task");
+    }
 }
